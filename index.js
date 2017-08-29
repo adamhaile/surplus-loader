072a38e15fbce71b84c1dcdc1ff7d3d54167c4cb
Adam Haile <adam.haile@gmail.com>
2017-08-28 23:59:15 -0400

    add sourcemap support

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 "use strict";
 
-var preprocessor = require("surplus-preprocessor");
+var preprocessor = require("surplus-preprocessor"),
+    sourcemap = require('source-map');
 
-module.exports = function preprocess(content) {
-    return preprocessor.preprocess(content, { });
+module.exports = function preprocess(content, incomingMap) {
+    var { src, map } = preprocessor.preprocess(content, { sourcemap: "extract" });
+
+    map.sources[0] = this.resourcePath;
+
+    if (incomingMap) {
+        // use source-map to combine incoming map with surplus-preprocessor's
+        var gen = sourcemap.SourceMapGenerator.fromSourceMap(new sourcemap.SourceMapConsumer(map));
+        gen.applySourceMap(new sourcemap.SourceMapConsumer(incomingMap));
+        map = gen.toJSON();
+    }
+
+    this.callback(null, src, map);
 };
